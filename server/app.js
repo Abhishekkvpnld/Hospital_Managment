68b21beabcaa7c8ca374fbb769c822b406a5a010
Abhishekkv <abhishekkvpnld@gmail.com>
2024-08-02 16:08:11 +0530

    database and cloudinary setup

--- server/app.js
+++ server/app.js
@@ -1,19 +1,42 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+import dbConnection from "./config/database.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import cloudinaryConfig from "./config/cloudinary.js";
+
+dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/tmp/"
+}));
 
-
-app.get("/",(req,res)=>{
-    res.send("server running...🚀")
+app.get("/", (req, res) => {
+  res.send("server running...🚀");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`);
+cloudinaryConfig();
+
+dbConnection().then((res) => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 🚀`);
+  });
 });
