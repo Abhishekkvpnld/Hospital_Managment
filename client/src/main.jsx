7fccae9d7c895f925990ceea3701227695500888
Abhishekkv <abhishekkvpnld@gmail.com>
2024-08-09 13:24:02 +0530

    routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createContext } from 'react'
+
+export const context = createContext({ isAuthenticated: false });
+
+export const AppWrapper = () => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
+
+  return (
+    <context.Provider value={{
+      isAuthenticated, setIsAuthenticated, user,
+      setUser
+    }}>
+      <App />
+    </context.Provider>
+  )
+
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppWrapper />
   </StrictMode>,
 )
