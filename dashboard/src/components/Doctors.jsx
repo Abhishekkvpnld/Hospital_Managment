6b995e3754b9e8451df3133ce9d32247151d9972
Abhishekkv <abhishekkvpnld@gmail.com>
2024-08-19 00:04:13 +0530

    doctor page completed

--- dashboard/src/components/Doctors.jsx
+++ dashboard/src/components/Doctors.jsx
@@ -33,8 +33,42 @@ const Doctors = () => {
     return <Navigate to={"/login"} />
   }
 
+  console.log(doctors)
+
   return (
-    <div>Doctors</div>
+    <section className="doctor_page_details">
+      <h1>DOCTORS</h1>
+      <div className="banner">
+
+        {
+          doctors && doctors.length > 0 ? (
+
+            doctors.map((doc, index) => {
+              return (
+                <div className="card" key={index}>
+                  <div className="doc_profile">
+                    <img src={doc?.docAvatar && doc.docAvatar.url} alt="img" className="doc_img" />
+                    <h3>{doc?.firstName} {doc?.lastName}</h3>
+                  </div>
+
+                  <div className="doc_details">
+                    <p> <span style={{fontWeight:"bold"}}>Email :</span> {doc?.email}</p>
+                    <p> <span style={{fontWeight:"bold"}}>Phone :</span> {doc?.phone}</p>
+                    <p> <span style={{fontWeight:"bold"}}>DOB :</span> {doc?.dob.substring(0,10)}</p>
+                    <p style={{ textDecoration: "underline", color: "blue"}}> <span style={{fontWeight:"bold"}}>Department :</span> {doc?.doctorDepartment}</p>
+                    <p style={{fontWeight:"bold"}}><span>Gender : </span>{doc?.gender}</p>
+                  </div>
+                </div>
+              )
+            })
+          ) : (
+            <h1>No Registered Doctor Available</h1>
+          )
+        }
+
+      </div>
+
+    </section >
   )
 }
 
